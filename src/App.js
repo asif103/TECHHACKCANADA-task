2a87a5d9de7b0b6080c00210d382aa0a1e200828
Mohammed Asif <asif.cse473@gmail.com>
2021-07-12 16:53:23 +0600

    initialize

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import NavBar from './components/Header/NavBar/NavBar';
+import Movies from './components/Movies/Movies';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar></NavBar>
+        <Switch>
+          <Route path="/" exact>
+            {/* <About /> */}
+          </Route>
+          <Route path="/movies">
+            <Movies></Movies>
+          </Route>
+          <Route path="/series">
+            {/* <Home /> */}
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
